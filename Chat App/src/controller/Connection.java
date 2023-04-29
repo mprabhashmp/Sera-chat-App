06439d67399b549440e13794df30efe22b593972
mprabhashmp <120329245+mprabhashmp@users.noreply.github.com>
2023-04-29 11:51:54 +0530

    xksjs

---
+++ Chat App/src/controller/Connection.java
@@ -0,0 +1,28 @@
+
+package controller;
+
+
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.SessionFactory;
+
+
+public class Connection {
+
+    private static final SessionFactory sessionFactory;
+    
+    static {
+        try {
+            // Create the SessionFactory from standard (hibernate.cfg.xml) 
+            // config file.
+            sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            // Log the exception. 
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+    
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
